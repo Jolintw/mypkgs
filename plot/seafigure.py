aab782373bf837e114c5b0b6c556d19c17131275
Jolin <b07209039@gmail.com>
2024-09-25 15:08:19 +0800

    use _plot to control pcolormesh or contourf

--- plot/seafigure.py
+++ plot/seafigure.py
@@ -6,7 +6,13 @@ xlim = [118, 123]
 ylim = [20.5, 27]
 dpi = 200
 
-def SSTplot(lon, lat, SST = None, terrain = None, xlim = xlim, ylim = ylim, ticksitvl = [None, None]):
+def _plot(PB, varname, plottype):
+    if plottype == "contourf":
+        return PB.contourf(varname, colorkey=varname)
+    elif plottype == "pcolormesh":
+        return PB.pcolormesh(varname, colorkey=varname)
+
+def SSTplot(lon, lat, SST = None, terrain = None, xlim = xlim, ylim = ylim, ticksitvl = [None, None], plottype = "contourf"):
     MP = MapPlotter(figsize=figsize, dpi=dpi)
     PB2 = Paintbox_2D(field=dict(terrain=terrain,SST=SST), X=lon, Y=lat, fig=MP.fig, ax=MP.ax, ft=MP.fontsize)
     
@@ -14,7 +20,7 @@ def SSTplot(lon, lat, SST = None, terrain = None, xlim = xlim, ylim = ylim, tick
         im, cb = PB2.contourf("terrain", colorkey="terrain")
         cb.remove()
     if not SST is None:
-        PB2.contourf("SST", colorkey="SST")
+        _plot(PB2, "SST", plottype)
     # MP.coastlines()
     MP.setlatlonticks(ticksitvl=ticksitvl, xlim=xlim, ylim=ylim)
     return MP, PB2
